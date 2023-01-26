4adc93af6bd39537d9c4108ea4264d84aadaa1b7
haechilim <haechilim@gmail.com>
2023-01-26 10:35:10 +0900

    favicon 변경 프로젝트 기본 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { Inter } from '@next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <></>
+    <>
+      <Head>
+        <title>Instagram</title>
+      </Head>
+    </>
   )
 }
